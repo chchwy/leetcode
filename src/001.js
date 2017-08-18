a07fc2d07d23c146e0275bad500da8916fbacae3
Matt <chchwy@gmail.com>
2017-08-18 10:57:49 +1000

    Update 001.js

--- src/001.js
+++ src/001.js
@@ -6,14 +6,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    var i = 0;
-    var j = 0;
-    for ( i = 0; i < nums.length; ++i )
-    {
-        for ( j = i + 1; j < nums.length; ++j )
-        {
-            if ( nums[i] + nums[j] === target )
-            {
+    let i = 0;
+    let j = 0;
+    for ( i = 0; i < nums.length; ++i ) {
+        for ( j = i + 1; j < nums.length; ++j ) {
+            if ( nums[i] + nums[j] === target ) {
                 return [ i, j ];
             }
         }
