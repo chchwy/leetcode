49b10787afe7b3847be204c76ceaee1610b587e0
Matt Chang <chchwy@gmail.com>
2018-08-21 10:06:15 +1000

    Add 035-search-insert-position.cpp

---
+++ src/035-search-insert-position.cpp
@@ -0,0 +1,26 @@
+// Classic binary search 
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int upper = nums.size() - 1;
+        int lower = 0;
+        
+        int m = (upper + lower) / 2;
+        while(upper >= lower) {
+            
+            if (nums[m] == target) return m;
+            if (nums[m] > target)
+                upper = m - 1;
+            else // nums[m]  < target
+                lower = m + 1;
+            
+            m = (upper + lower) / 2;
+        }
+        
+        if (target > nums[m])
+            return m + 1;
+        else if (target < nums[m])
+            return m;
+        
+    }
+};
