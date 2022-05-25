eadbc00ce2ab3793d030594c2a6b2ed6469f65ea
Matt Chang <chchwy@gmail.com>
2022-05-25 13:08:13 +0800

    704 binary_search (while-loop version)

--- leetcode/0704-binary-search.cpp
+++ leetcode/0704-binary-search.cpp
@@ -6,7 +6,7 @@ using std::vector;
 /**
  704. Binary Search
 */
-class Solution
+class Solution1 // Recursive binary search
 {
 public:
     
@@ -36,10 +36,43 @@ public:
     }
 };
 
+// While-loop Binary Search
+class Solution2
+{
+public:
+    int binary_search(const vector<int>& nums, int target, int left, int right)
+    {
+        int mid = (left + right) / 2;
+        while (left <= right)
+        {
+            if (target == nums[mid])
+            {
+                return mid;
+            }
+            if (target < nums[mid])
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+            mid = (left + right) / 2;
+        }
+        return -1;
+    }
+
+    int search(vector<int>& nums, int target)
+    {
+        return binary_search(nums, target, 0, nums.size() - 1);
+    }
+};
+
 int main()
 {
 	vector<int> input {-1, 0, 3, 5, 9, 12};
-    Solution sln;
+    Solution1 sln;
 	printf("%d\n", sln.search(input, 9));
+    printf("%d\n", sln.search(input, 2));
 	return 0;
 }
